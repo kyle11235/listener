b70ecb8824f4597cdf6d259f684b9d35e616042a
someone <someone@example.com>
2020-07-17 12:39:23 +0800

    clear

--- listener/main.go
+++ listener/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/kyleoracle/listener/config"
@@ -30,44 +31,6 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
 		// fmt.Printf("body=\n\n%s\n", reqBody)
-
-		tx := gjson.GetBytes(reqBody, `eventMsg.0.payload.action.proposalResponsePayload.extension.results.nsRwset.#(namespace=="orderchain").rwset.writes.0.value.data`)
-		fmt.Printf("tx=%s\n\n", tx)
-
-		txBytes := []byte(tx.Raw) // string -> bytes = []byte(tx.Raw), bytes -> string = string(data[:])
-
-		// data
-		order := &model.OrderNetsuite{
-			URI: "salesorder",
-		}
-		err := json.Unmarshal(txBytes, &order.Data)
-		if err != nil {
-			fmt.Println("Unmarshal err=", err.Error())
-		}
-
-		orderBytes, err := json.Marshal(order)
-		if err != nil {
-			fmt.Println("Marshal err=", err.Error())
-		}
-
-		fmt.Printf("order bytes=%s\n\n", orderBytes)
-
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(orderBytes)) // interface io.Reader, read bytes
-		req.Header.Set("Content-Type", "application/json")
-
-		// auth
-		req.Header.Set("Authorization", config.Config["auth"])
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			// panic(err)
-			fmt.Println("post err=", err.Error())
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("post response Status=", resp.Status)
-
 	}))
 
 	// Routes
@@ -77,6 +40,49 @@ func main() {
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
-func fn(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func fn(context echo.Context) error {
+	bodyBytes, _ := ioutil.ReadAll(context.Request().Body)
+	go post(bodyBytes)
+	fmt.Println("fn sends response")
+	return context.String(http.StatusOK, "Hello, World!")
+}
+
+func post(bodyBytes []byte) {
+
+	tx := gjson.GetBytes(bodyBytes, `eventMsg.0.payload.action.proposalResponsePayload.extension.results.nsRwset.#(namespace=="orderchain").rwset.writes.0.value.data`)
+	fmt.Printf("tx=%s\n\n", tx)
+
+	txBytes := []byte(tx.Raw)
+
+	// data
+	order := &model.OrderNetsuite{
+		URI: "salesorder",
+	}
+	err := json.Unmarshal(txBytes, &order.Data)
+	if err != nil {
+		fmt.Println("Unmarshal err=", err.Error())
+	}
+
+	orderBytes, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("Marshal err=", err.Error())
+	}
+
+	fmt.Printf("order bytes=%s\n\n", orderBytes)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(orderBytes)) // interface io.Reader, read bytes
+	req.Header.Set("Content-Type", "application/json")
+
+	// auth
+	req.Header.Set("Authorization", config.Config["auth"])
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// panic(err)
+		fmt.Println("post err=", err.Error())
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("post response Status=", resp.Status)
 }
